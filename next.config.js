c1e6891ed8f9e30039f73019eae6d93471a45568
eugevanz <eugevanz@gmail.com>
2022-04-23 02:00:45 +0200

    ReferenceError: nextConfig is not defined

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const plugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
 
-// manage i18n
-if (process.env.EXPORT !== 'true') {
-  nextConfig.i18n = {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  };
-}
+module.exports = nextConfig;
